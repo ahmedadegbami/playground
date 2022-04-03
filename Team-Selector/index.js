793f81ce8ddc4cb0145707b6f525bf240b923835
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-03 09:45:23 +0200

    updated

--- Team-Selector/index.js
+++ Team-Selector/index.js
@@ -1,10 +1,18 @@
+
+let inputNode = document.getElementById("member-Input");
+let ulwaitingNode = document.getElementById("waiting-List");
+
+let teamNumber = document.getElementById("teams-number");
+let rowNode = document.getElementById("teams-row");
+
+
 function addNewmember(event) {
     event.preventDefault();
-    let inputNode = document.getElementById("member-Input").value;
-    let ulwaitingNode = document.getElementById("waiting-List");
+    
+    
     let listNode = document.createElement("li");
     listNode.classList.add("list-group-item");
-    listNode.innerHTML = inputNode;
+    listNode.innerHTML = inputNode.value;
     ulwaitingNode.appendChild(listNode);
 }
 
@@ -13,19 +21,64 @@ window.onload = function () {
 }
 
 
-let teamNumber = document.getElementById("teams-number");
-let rowNode = document.getElementById("teams-row");
+
 
 function createTeams(){
     
     for (let i=0; i< teamNumber.innerHTML; i++){
-        rowNode.innerHTML += `<div class="col-3">
+        rowNode.innerHTML += `<div class="col-3 ">
           <div class="card bg-primary">
             <div class="card-header">Team ${i + 1}</div>
-            <ul class="list-group list-group-flush"></ul>
+            <ul  class="list-group list-group-flush newlist"></ul>
           </div>
         </div>`
         
     }
 
+}
+
+let team = document.getElementById("teams-number");
+function addteam() {
+    
+    team.innerHTML = parseInt(team.innerHTML) + 1;
+    rowNode.innerHTML += `<div class="col-3 ">
+          <div class="card bg-primary">
+            <div class="card-header">Team ${team.innerHTML}</div>
+            <ul  class="list-group list-group-flush newlist"></ul>
+          </div>
+        </div>`
+        }
+
+
+function removeteam() { 
+  let teamsList = document.querySelectorAll(".newlist");
+  let lastTeam = teamsList[teamsList.length - 1];
+  let teamMembers = lastTeam.querySelectorAll("li");
+  for (let i = 0; i < teamMembers.length; i++) {
+    let listNode = document.createElement("li");
+    listNode.classList.add("list-group-item");
+    listNode.innerHTML = teamMembers[i].innerHTML;
+    ulwaitingNode.appendChild(listNode);
+  }
+
+
+
+  team.innerHTML = parseInt(team.innerHTML) - 1;
+  rowNode.removeChild(rowNode.lastChild);
+}
+
+
+function assign() {
+  for (let i = 0; i < 1; i++) {
+  let person = ulwaitingNode.children[0];
+  if(!person) return
+
+  let teams = document.querySelectorAll(".newlist");
+
+
+  let randomTeam = Math.floor(Math.random() * teams.length);
+
+  teams[randomTeam].appendChild(person);
+  
+}
 }
