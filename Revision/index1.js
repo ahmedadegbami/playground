143d22e0e79e26f1864554eae99acea92e0a11aa
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-13 01:35:15 +0200

    ok

--- Revision/index1.js
+++ Revision/index1.js
@@ -1,46 +1,112 @@
+let findSong = (event) => {
+    
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem', {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(songs=> {
+    
+            let songsList = songs.data
+            console.log(songsList)
+    
+            let row = document.querySelector(".row")
 
-// window.onload = () => {
-//     fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem', {
-//         method: "GET"
-//     })
-//         .then(response => response.json())
-//         .then(songs=> {
+            row.innerHTML = songsList.filter(song => song.title.includes(event.target.value))
+            .map(song => 
+              
+               `<div class="col-3">
+               <div class="card">
+            
+                    <img src=${song.album.cover_small} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">${song.title}</h5>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                    </div>`).join("");
+
+        
+        }).catch(err => console.error(err));
+
+}
+
+const loadSongs = () => {
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem', {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(songs=> {
     
-//             let songsList = songs.data
-//             console.log(songsList)
+            let songsList = songs.data
+            console.log(songsList)
     
-//             let row = document.querySelector(".row")
+            let row = document.querySelector(".row")
+           row.innerHTML = songsList.
+            map(song => 
+              
+               `<div class="col-3">
+               <div class="card">
+            
+                    <img src=${song.album.cover_small} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">${song.title}</h5>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                    </div>`).join("");
+
+
+             }).catch(err => console.error(err));
+
+}
             
     
-//             songsList.forEach(song => {
-//                 let col = document.createElement("div")
-//                 col.classList.add("col-3")
-//                 col.innerHTML = `<div class="card">
+            // songsList.forEach(song => {
+            //     let col = document.createElement("div")
+            //     col.classList.add("col-3")
+            //     col.innerHTML = `<div class="card">
             
-//                     <img src=${song.album.cover_small} class="card-img-top" alt="..." />
-//                     <div class="card-body">
-//                         <h5 class="card-title">${song.title}</h5>
-//                         <a href="#" class="btn btn-primary">Go somewhere</a>
-//                     </div>
-//                     </div>`
+            //         <img src=${song.album.cover_small} class="card-img-top" alt="..." />
+            //         <div class="card-body">
+            //             <h5 class="card-title">${song.title}</h5>
+            //             <a href="#" class="btn btn-primary">Go somewhere</a>
+            //         </div>
+            //         </div>`
     
-//                     row.appendChild(col)
+            //         row.appendChild(col)
                 
-//             });
-        
-        
-//         }).catch(err => console.error(err));
+            // });
 
 
-// }
+window.onload = () => {
+    loadSongs()
+    
+}
+
+// const arrOfPeople = [
+//     {name: "Ahmed", age: 50, salary: 360},
+//     {name: "Adam", age: 20, salary: 30},
+//     {name: "Shola", age: 30, salary: 500},
+//     {name: "bOLU", age: 160, salary: 300}
+// ]
+
+// let arr = arrOfPeople.map(obj => obj.age)
+// console.log(arr)
 
 
-const arrOfPeople = [
-    {name: "Ahmed", age: 50, salary: 360},
-    {name: "Adam", age: 20, salary: 30},
-    {name: "Shola", age: 30, salary: 500},
-    {name: "bOLU", age: 160, salary: 300}
-]
+// let agba = arrOfPeople.filter(obj => obj.age > 60)
+// console.log(agba)
 
-let arr = arrOfPeople.map(obj => obj.age)
-console.log(arr)
+
+
+// let arrangeByAge = arrOfPeople.sort((a,b) => {
+//     if(a.age < b.age){
+//         return -1
+//     } else {
+//         return 1
+//     }
+// })
+// console.log(arrangeByAge)
+
+
+// console.log(arrOfPeople.reduce((acc, curr) => acc.concat(curr.age), []))
