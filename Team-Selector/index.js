062e290fee07c97e2be9677461b3d20c281e68c3
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-20 08:29:00 +0200

    OK

--- Team-Selector/index.js
+++ Team-Selector/index.js
@@ -1,56 +1,45 @@
-
 let inputNode = document.getElementById("member-Input");
 let ulwaitingNode = document.getElementById("waiting-List");
 
 let teamNumber = document.getElementById("teams-number");
 let rowNode = document.getElementById("teams-row");
 
-
 function addNewmember(event) {
-    event.preventDefault();
-    
-    
-    let listNode = document.createElement("li");
-    listNode.classList.add("list-group-item");
-    listNode.innerHTML = inputNode.value;
-    ulwaitingNode.appendChild(listNode);
+  event.preventDefault();
+
+  let listNode = document.createElement("li");
+  listNode.classList.add("list-group-item");
+  listNode.innerHTML = inputNode.value;
+  ulwaitingNode.appendChild(listNode);
 }
 
-window.onload = function () {
-    createTeams();
-}
+// window.onload = function () {
+//     createTeams();
+// }
 
-
-
-
-function createTeams(){
-    
-    for (let i=0; i< teamNumber.innerHTML; i++){
-        rowNode.innerHTML += `<div class="col-3 ">
-          <div class="card bg-primary">
-            <div class="card-header">Team ${i + 1}</div>
-            <ul  class="list-group list-group-flush newlist"></ul>
-          </div>
-        </div>`
-        
-    }
-
-}
+// function createTeams() {
+//   for (let i = 0; i < teamNumber.innerHTML; i++) {
+//     rowNode.innerHTML += `<div class="col-3 ">
+//           <div class="card bg-primary">
+//             <div class="card-header">Team ${i + 1}</div>
+//             <ul  class="list-group list-group-flush newlist"></ul>
+//           </div>
+//         </div>`;
+//   }
+// }
 
 let team = document.getElementById("teams-number");
 function addteam() {
-    
-    team.innerHTML = parseInt(team.innerHTML) + 1;
-    rowNode.innerHTML += `<div class="col-3 ">
+  team.innerHTML = parseInt(team.innerHTML) + 1;
+  rowNode.innerHTML += `<div class="col-3 ">
           <div class="card bg-primary">
             <div class="card-header">Team ${team.innerHTML}</div>
             <ul  class="list-group list-group-flush newlist"></ul>
           </div>
-        </div>`
-        }
+        </div>`;
+}
 
-
-function removeteam() { 
+function removeteam() {
   let teamsList = document.querySelectorAll(".newlist");
   let lastTeam = teamsList[teamsList.length - 1];
   let teamMembers = lastTeam.querySelectorAll("li");
@@ -61,24 +50,19 @@ function removeteam() {
     ulwaitingNode.appendChild(listNode);
   }
 
-
-
   team.innerHTML = parseInt(team.innerHTML) - 1;
   rowNode.removeChild(rowNode.lastChild);
 }
 
-
 function assign() {
   for (let i = 0; i < 1; i++) {
-  let person = ulwaitingNode.children[0];
-  if(!person) return
+    let person = ulwaitingNode.children[0];
+    if (!person) return;
 
-  let teams = document.querySelectorAll(".newlist");
+    let teams = document.querySelectorAll(".newlist");
 
+    let randomTeam = Math.floor(Math.random() * teams.length);
 
-  let randomTeam = Math.floor(Math.random() * teams.length);
-
-  teams[randomTeam].appendChild(person);
-  
-}
+    teams[randomTeam].appendChild(person);
+  }
 }
